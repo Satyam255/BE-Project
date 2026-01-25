c0605c52d6c9167730c37e9d2c541088b1b434f4
Tejas Birje <tejasbirje30@gmail.com>
2026-01-25 14:28:17 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express"
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+import connectDB from "./config/db.js"
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config();
+
+const app = express();
+
+// CORS
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
+// Connect DB
+connectDB();
+
+// Middlewares
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Server uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}.`)
+})
+
