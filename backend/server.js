12b2594bbf04604e30e395540ac5393a0fd36b8b
Satyam255 <satyambindhani25@gmail.com>
2026-04-15 01:43:27 +0530

    Server Deployment config

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,12 @@ const server = http.createServer(app);
 // );
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://usehirely.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://usehirely.vercel.app",
+      "https://www.hire-ly.tech",
+      "hire-ly.tech",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
